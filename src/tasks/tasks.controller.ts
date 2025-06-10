d1de2f22f09ccbd4e224e48775cb23a47b72b659
BerbardoMoschen <bernardomoschen.dev@gmail.com>
2025-06-10 15:39:45 -0300

    wip:createTaks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks() {
+  getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    const res = this.tasksService.createTask(title, description);
+    console.log(res);
+    return res;
   }
 }
