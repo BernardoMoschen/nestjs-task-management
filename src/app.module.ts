53c503157d5f20c16048eb00abe363a48b34b798
BerbardoMoschen <bernardomoschen.dev@gmail.com>
2025-06-10 17:11:42 -0300

    wip: chore database connection module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
-import { TasksService } from './tasks/tasks.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [TasksModule],
-  providers: [TasksService],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'nestjs-tasks-management',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
