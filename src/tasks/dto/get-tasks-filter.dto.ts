dbb6d3972815971c96eae0fdccf620a85b73f4e2
BerbardoMoschen <bernardomoschen.dev@gmail.com>
2025-06-10 16:52:28 -0300

    wip:validator update

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,13 @@
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
   search?: string;
+
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
 }
